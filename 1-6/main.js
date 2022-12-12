af4da495f871d29881ea8e679c3c640a180d46e2
kumanomi0620 <hata0620@yahoo.co.jp>
2022-12-13 01:29:25 +0900

    課題提出１−６
    よろしくお願いします。

---
+++ 1-6/main.js
@@ -0,0 +1,13 @@
+let scores =[10,15,20,25];
+for(let i=0; i<scores.length; i++){
+  //conosole.log(scores[i]);
+  //変数iが偶数の場合
+  if(scores[i]%2===0){
+    //偶数として画面出力
+    console.log(scores[i]+'偶数です。');
+  }
+}
+let car = {gass:20.5,num:1234};
+console.log(`ガソリンは${car.gass}です`)
+console.log(`ナンバーは${car.num}です`)
+
